dbea2aacf396b86bc646e537ac1b6cf94db67485
liuxiaozhong <>
2021-02-05 12:16:35 +0800

    update android

--- Call-Android/app/src/main/java/org/ar/call/RtmManager.kt
+++ Call-Android/app/src/main/java/org/ar/call/RtmManager.kt
@@ -1,30 +1,61 @@
-package org.ar.call.utils
+package org.ar.call
 
+import android.app.Notification
+import android.app.NotificationManager
+import android.app.NotificationManager.IMPORTANCE_HIGH
+import android.app.PendingIntent
 import android.content.Context
+import android.content.Intent
+import android.net.Uri
+import android.os.Handler
+import android.os.Looper
+import android.provider.Settings
+import com.ycbjie.notificationlib.NotificationUtils
+import org.ar.call.multi.MultiCallActivity
+import org.ar.call.p2p.VideoActivity
 import org.ar.rtm.*
 import org.json.JSONException
 import org.json.JSONObject
 
-object RTManager{
 
-    var rtmClient: RtmClient? = null
-    var rtmCallManager:RtmCallManager? = null
-    var rtmChannel:RtmChannel? = null
+class RtmManager private constructor(){
+
+    private var rtmClient : RtmClient? = null
+    private var callManager : RtmCallManager? = null
+    private var rtmChannel : RtmChannel? =null
     private val rtmEventList = mutableListOf<RtmClientListener>()
     private val callEventList = mutableListOf<RtmCallEventListener>()
     private val channelEventList = mutableListOf<RtmChannelListener>()
-    var remoteInvitation:RemoteInvitation? =null
-    var localInvitation:LocalInvitation? = null
+    private var remoteInvitation: RemoteInvitation? =null
+    public var localInvitation: LocalInvitation? = null
+    private var notificationUtils: NotificationUtils? = null
 
-    fun init(context: Context, appId: String){
-        rtmClient = RtmClient.createInstance(context, appId, RtmEvent)
-        rtmCallManager = rtmClient?.rtmCallManager
-        rtmCallManager?.setEventListener(CallEvent)
+
+    fun getRtmClient():RtmClient?{
+        return rtmClient
+    }
+
+    fun getCallManager():RtmCallManager?{
+        return callManager
+    }
+
+    fun getRtmChannel():RtmChannel?{
+        return rtmChannel
+    }
+
+    fun getRemoteInvitation():RemoteInvitation?{
+        return remoteInvitation
+    }
+
+    fun init(context: Context){
+        rtmClient = RtmClient.createInstance(context, BuildConfig.APPID, RtmEvent())
+        callManager = rtmClient?.rtmCallManager
+        callManager?.setEventListener(CallEvent())
     }
 
     fun createChannel(channelId: String): RtmChannel? {
         return if (rtmChannel == null){
-            rtmChannel= rtmClient?.createChannel(channelId, ChannelEvent)
+            rtmChannel = rtmClient?.createChannel(channelId, ChannelEvent())
             rtmChannel
         }else{
             rtmChannel
@@ -36,37 +67,8 @@ object RTManager{
         rtmChannel?.release()
         rtmChannel = null
     }
-
-
-    private object RtmEvent: RtmClientListener {
-        override fun onConnectionStateChanged(state: Int, reason: Int) {
-            if (state == 4){
-                remoteInvitation = null
-            }
-            rtmEventList.forEach {
-                it.onConnectionStateChanged(state, reason)
-            }
-        }
-
-        override fun onMessageReceived(var1: RtmMessage?, var2: String?) {
-            rtmEventList.forEach {
-                it.onMessageReceived(var1, var2)
-            }
-        }
-
-        override fun onTokenExpired() {
-        }
-
-        override fun onPeersOnlineStatusChanged(var1: MutableMap<String, Int>?) {
-            rtmEventList.forEach {
-                it.onPeersOnlineStatusChanged(var1)
-            }
-        }
-
-    }
-
-    private object CallEvent : RtmCallEventListener{
-
+    
+    private inner class CallEvent : RtmCallEventListener{
         //返回给主叫的回调：被叫已收到呼叫邀请。
         override fun onLocalInvitationReceivedByPeer(var1: LocalInvitation?) {
             callEventList.forEach {
@@ -104,6 +106,40 @@ object RTManager{
         //返回给被叫的回调：收到一条呼叫邀请。SDK 会同时返回一个 RemoteInvitation 对象供被叫管理。
         override fun onRemoteInvitationReceived(var1: RemoteInvitation?) {
             remoteInvitation = var1
+            Handler(Looper.getMainLooper()).post {
+                if (CallApp.callApp.mActivityCount==0){//in back
+                    try {
+                        val jsonObject = JSONObject(remoteInvitation!!.content)
+                        val isConference = jsonObject.getBoolean("Conference")
+                        val caller = remoteInvitation?.callerId
+                        val resultIntent = Intent().apply {
+                            if (!isConference) {
+                                CallApp.callApp.curActivity?.let { setClass(it, VideoActivity::class.java) }
+                                putExtra("RecCall", true)
+                            } else {
+                                CallApp.callApp.curActivity?.let { setClass(it, MultiCallActivity::class.java) }
+                                putExtra("RecCall", true)
+                            }
+//                            addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+                        }
+                        val vibrate = longArrayOf(0, 500, 1000, 1500)
+                        val resultPendingIntent = PendingIntent.getActivity(CallApp.callApp.curActivity, 3, resultIntent, PendingIntent.FLAG_UPDATE_CURRENT)
+                        notificationUtils = NotificationUtils(CallApp.callApp.curActivity)
+                        notificationUtils?.let {
+                            it.setOngoing(false) //设置内容点击处理intent
+                            it.setContentIntent(resultPendingIntent) //设置状态栏的标题
+                            it.setSound(Uri.parse("android.resource://org.ar.call/"+R.raw.video_request)) //设置优先级
+                            it.setVibrate(vibrate)
+                            it.setPriority(NotificationManager.IMPORTANCE_HIGH) //自定义震动效果
+                            it.sendNotification(3, "收到${caller}的呼叫", "", R.drawable.img_accept)
+                        } //让通知左右滑的时候是否可以取消通知
+                                 } catch (e: JSONException) {
+                        e.printStackTrace()
+                    }
+
+
+                }
+            }
             callEventList.forEach {
                 it.onRemoteInvitationReceived(var1)
             }
@@ -114,6 +150,7 @@ object RTManager{
                 it.onRemoteInvitationAccepted(var1)
             }
             remoteInvitation = null
+            notificationUtils?.clearNotification()
         }
 
         //返回给被叫的回调：拒绝呼叫邀请成功
@@ -122,6 +159,7 @@ object RTManager{
                 it.onRemoteInvitationRefused(var1)
             }
             remoteInvitation = null
+            notificationUtils?.clearNotification()
         }
 
         override fun onRemoteInvitationCanceled(var1: RemoteInvitation?) {
@@ -129,6 +167,7 @@ object RTManager{
                 it.onRemoteInvitationCanceled(var1)
             }
             remoteInvitation = null
+            notificationUtils?.clearNotification()
         }
 
         override fun onRemoteInvitationFailure(var1: RemoteInvitation?, var2: Int) {
@@ -136,11 +175,12 @@ object RTManager{
                 it.onRemoteInvitationFailure(var1, var2)
             }
             remoteInvitation = null
+            notificationUtils?.clearNotification()
         }
 
     }
 
-    private object ChannelEvent : RtmChannelListener{
+    private inner class ChannelEvent : RtmChannelListener{
         override fun onMemberCountUpdated(var1: Int) {
             channelEventList.forEach {
                 it.onMemberCountUpdated(var1)
@@ -172,6 +212,30 @@ object RTManager{
         }
     }
 
+    private inner class RtmEvent: RtmClientListener {
+        override fun onConnectionStateChanged(state: Int, reason: Int) {
+            rtmEventList.forEach {
+                it.onConnectionStateChanged(state, reason)
+            }
+        }
+
+        override fun onMessageReceived(var1: RtmMessage?, var2: String?) {
+            rtmEventList.forEach {
+                it.onMessageReceived(var1, var2)
+            }
+        }
+
+        override fun onTokenExpired() {
+        }
+
+        override fun onPeersOnlineStatusChanged(var1: MutableMap<String, Int>?) {
+            rtmEventList.forEach {
+                it.onPeersOnlineStatusChanged(var1)
+            }
+        }
+
+    }
+    
     fun registerCallListener(callEventListener: RtmCallEventListener){
         if (callEventListener !in callEventList){
             callEventList.add(callEventListener)
@@ -201,5 +265,18 @@ object RTManager{
     fun unRegisterChannelEvent(channelListener: RtmChannelListener){
         channelEventList.remove(channelListener)
     }
+
+    fun release(){
+        rtmClient?.release()
+    }
+
+
+    companion object {
+        val instance: RtmManager by lazy() {
+            RtmManager()
+        }
+    }
+
+
 
 }
---
+++ Call-Android/app/src/main/java/org/ar/call/utils/Constans.kt
@@ -0,0 +1,15 @@
+package org.ar.call.utils
+
+object Constans {
+    const val CALL_MODE = "mode"
+    const val VIDEO_MODE = 0
+    const val AUDIO_MODE = 1
+    const val SINGLE_MODE = 0
+    const val MEETING_MODE = 1
+    const val OPEN_DENOISE = "denoise"
+    const val KEY_SINGLE_CALL = "p2p" //p2p通话
+    const val KEY_OPEN_CAMERA = "isOpenCamera"
+    const val KEY_OPEN_MIC = "isOpenMic"
+    const val KEY_FRAME = "frame"
+    const val KEY_DIMENS = "dimensions"
+}
