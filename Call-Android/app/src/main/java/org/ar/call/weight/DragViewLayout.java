c1b8972091f54508e2421b1fb45a728349ff9188
liuxiaozhong <>
2021-08-20 21:36:32 +0800

    update android

--- Call-Android/app/src/main/java/org/ar/call/weight/DragViewLayout.java
+++ Call-Android/app/src/main/java/org/ar/call/weight/DragViewLayout.java
@@ -1,12 +1,10 @@
 package org.ar.call.weight;
 
 import android.content.Context;
-import android.text.TextUtils;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.RelativeLayout;
 
 import androidx.customview.widget.ViewDragHelper;
